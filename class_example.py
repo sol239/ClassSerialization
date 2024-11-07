7d65f95241da8f032b4514782076c6b5b7c9bf81
David Valek <david.valek17@gmail.com>
2024-11-07 17:34:40 +0100

    Finish (de)serilization functions

--- class_example.py
+++ class_example.py
@@ -1,6 +1,8 @@
 
 class Book:
-    title: str
-    authors: list
-    page_count:int
-    price:float
+   
+    def __init__(self, title:str, authors:list, page_count:int, price:float):
+        self.title = title
+        self.authors = authors
+        self.page_count = page_count
+        self.price = price
--- serialization.py
+++ serialization.py
@@ -1,31 +1,18 @@
-import dataclasses
 import json
+from typing import Type, TypeVar, Optional
 
-def serialize_class(class_instance:object, save_file:bool = False, file_path:str = None) -> str:
-    json_string = json.dumps(object)
+T = TypeVar('T')
+
+def serialize(class_instance, save_file: bool = False, file_path: Optional[str] = None) -> str:
+    json_string = json.dumps(class_instance.__dict__)
     if (save_file):
         file = open(file_path, "w", encoding="utf-8")
         file.write(json_string)
         file.close
     return json_string
 
-def serialize_dataclass(dataclass_instance:object, save_file:bool = False, file_path:str = None):
-    json_string = json.dumps(object)
-    if (save_file):
-        file = open(file_path, "w", encoding="utf-8")
-        file.write(json_string)
-        file.close
-    return json_string
-
-def deserialize_dataclass(file_path:str) -> object:
-    file = open(file_path, "r", encoding="utf-8")
-    json_string = file.read()
-    file.close()
-    return json.loads(json_string)
-
-
-def deserialize_class(file_path:str) -> object:
-    file = open(file_path, "r", encoding="utf-8")
-    json_string = file.read()
-    file.close()
-    return json.loads(json_string)
+def deserialize(class_type: Type[T], file_path: str) -> T:
+    with open(file_path, "r", encoding="utf-8") as file:
+        json_data = json.load(file)
+    return class_type(**json_data)
+    
