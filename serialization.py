309953ccdb9ec31210251fe622b206cfd657afdf
David Valek <david.valek17@gmail.com>
2024-11-07 16:57:10 +0100

    Add method definitions

--- serialization.py
+++ serialization.py
@@ -1,9 +1,31 @@
 import dataclasses
+import json
 
-def serialize_class(class_instance:object, save_file:bool = False, file_path:str = None):
-    
-    return
+def serialize_class(class_instance:object, save_file:bool = False, file_path:str = None) -> str:
+    json_string = json.dumps(object)
+    if (save_file):
+        file = open(file_path, "w", encoding="utf-8")
+        file.write(json_string)
+        file.close
+    return json_string
 
 def serialize_dataclass(dataclass_instance:object, save_file:bool = False, file_path:str = None):
-    return
+    json_string = json.dumps(object)
+    if (save_file):
+        file = open(file_path, "w", encoding="utf-8")
+        file.write(json_string)
+        file.close
+    return json_string
 
+def deserialize_dataclass(file_path:str) -> object:
+    file = open(file_path, "r", encoding="utf-8")
+    json_string = file.read()
+    file.close()
+    return json.loads(json_string)
+
+
+def deserialize_class(file_path:str) -> object:
+    file = open(file_path, "r", encoding="utf-8")
+    json_string = file.read()
+    file.close()
+    return json.loads(json_string)
