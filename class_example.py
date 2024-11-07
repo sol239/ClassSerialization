7dd0d165e04caaf112d51fe1e46a623643f5a828
David Valek <david.valek17@gmail.com>
2024-11-07 16:50:34 +0100

    create serialization.py file with method for serialization

---
+++ class_example.py
@@ -0,0 +1,6 @@
+
+class Book:
+    title: str
+    authors: list
+    page_count:int
+    price:float
---
+++ dataclass_example.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Book:
+    title: str
+    authors: list
+    page_count:int
+    price:float
+    
+    
