7b33004065e4bff3682a123ea136cb5dac073390
David Valek <david.valek17@gmail.com>
2024-11-07 16:53:57 +0100

    Add methods for serialization declarations

---
+++ serialization.py
@@ -0,0 +1,9 @@
+import dataclasses
+
+def serialize_class(class_instance:object, save_file:bool = False, file_path:str = None):
+    
+    return
+
+def serialize_dataclass(dataclass_instance:object, save_file:bool = False, file_path:str = None):
+    return
+
