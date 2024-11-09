e2dde67829d338968d0878388f99888074fb07ff
David Valek <david.valek17@gmail.com>
2024-11-09 16:16:02 +0100

    Change error message

--- tests/mainTest.py
+++ tests/mainTest.py
@@ -7,13 +7,14 @@ class Painting:
     name:str
     price:float
     year_painted:int
-    
+
 class TestSerializationClass(unittest.TestCase):
+
     def test_serialization(self):
-        
         painting = Painting(name="Picture 1", price=29011.11, year_painted=1999)
-        
-        # data
+
         expected = """{"name": "Picture 1", "price": 29011.11, "year_painted": 1999}"""
         actual = serialize(painting)
-        self.assertEqual(expected, actual)
+        self.assertEqual(expected, actual, "Serialization failed")
+
+    
